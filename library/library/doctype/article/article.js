9b9c592e05e83745fed8e7ea0ea5cdfa2f15a550
wajid800 <wajidhazarvi1@gmail.com>
2025-10-18 18:41:17 +0500

    new change

--- library/library/doctype/article/article.js
+++ library/library/doctype/article/article.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2025, isfak and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Article", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Article", {
+	refresh(frm) {
+       frappe.message.show("Hello, welcome to the Article doctype!");
+	},
+});
